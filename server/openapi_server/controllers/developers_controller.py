9384375bb5db5cfb18b45d3a5878691d17415190
Tobias Brohl <tobrohl@gmail.com>
2019-05-25 14:24:09 +0200

    Api spec v3

--- server/openapi_server/controllers/developers_controller.py
+++ server/openapi_server/controllers/developers_controller.py
@@ -19,5 +19,5 @@ def get_bikes_internal(coordinates=None, limit=None):  # noqa: E501
     :rtype: BikeRet
     """
     if connexion.request.is_json:
-        coordinates = .from_dict(connexion.request.get_json())  # noqa: E501
+        coordinates = from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
