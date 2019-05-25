a2bf1d34ebf62f6c67bfa1f8ba3236575786a799
Felix <felixletsplay@outlook.de>
2019-05-25 16:38:14 +0200

    Simple server for getting bikes

--- server/openapi_server/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from openapi_server.models.bike import Bike
-from openapi_server.models.bike_ret import BikeRet
-from openapi_server.models.coordinates import Coordinates
