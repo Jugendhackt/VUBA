eab1eeda1ce7f5fb1a3595992c701ad6779f0a7c
Felix <felixletsplay@outlook.de>
2019-05-26 10:31:25 +0200

    Fix reponsive rendering

--- client/src/responsive.js
+++
@@ -1,7 +0,0 @@
-if (window.innerWidth < 500) {
-    document.getElementById('locationInput').style.width = ((window.innerWidth / 6) * 3) + "";
-    document.getElementById('setLocation').style.width = ((window.innerWidth / 6) * 1.5) + "";
-    document.getElementById('search').classList.add('mobile')
-} else {
-    document.getElementById('search').classList.add('desktop')
-}
