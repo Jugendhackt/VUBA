a158493b268fed5fb3385e1afe52e31b85ea450b
Felix <felixletsplay@outlook.de>
2020-05-13 16:22:07 +0200

    Fix Mobike

--- interface/provider/mobike.py
+++ interface/provider/mobike.py
@@ -9,15 +9,14 @@ class Mobike(ProviderInterface):
     def get_bikes(self, lat: float, lon: float, limit: int) -> [Bike]:
         data = {
             "latitude": lat,
-            "longitude": lon
+            "Longitude": lon
         }
         header= {
             "platform": "1",
-            "Content - Type": "application / x - www - form - urlencoded",
-            "User - Agent": "Mozilla / 5.0(Android 7.1.2; Pixel Build / NHG47Q) AppleWebKit / 537.36(KHTML, like Gecko) "
-                            "Chrome / 54.0.2840.9 NTENTBrowser / 3.7.0.496(IWireless - US) Mobile Safari / 537.36"
+            "content-type": "application/x-www-form-urlencoded",
+            "user-agent": "Mozilla/5.0 (Android 7.1.2; Pixel Build/NHG47Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.9 NTENTBrowser/3.7.0.496 (IWireless-US) Mobile Safari/537.36 (or whatever)"
         }
-        response = requests.post("http://app.mobike.com/api/nearby/v4/nearbyBikeInfo",
+        response = requests.post("http://global-n-mobike-g.mobike.com/api/nearby/v4/nearbyBikeInfo",
                                  headers=header,
                                  data=data)
         bikes = []
